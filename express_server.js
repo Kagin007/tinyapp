34417cd686f78a882317243ecee277b3f628a313
Adam Schulte <adamschulte148@hotmail.com>
2022-01-24 20:59:12 +0000

    adding submit/post functionality to our form

--- express_server.js
+++ express_server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+function generateRandomString() {
+  let sixDigitNum = Math.floor(Math.random()*1000000)
+  return sixDigitNum.toString()
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs')
 
@@ -27,10 +35,21 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars)
 })
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 
 
 app.listen(PORT, () => {
