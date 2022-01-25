a5bc53ffe275a6d59858f6403403863033c51cf8
Adam Schulte <adamschulte148@hotmail.com>
2022-01-25 00:55:10 +0000

    fixed the redirecting issue

--- express_server.js
+++ express_server.js
@@ -3,54 +3,60 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 function generateRandomString() {
   let sixDigitNum = Math.floor(Math.random()*1000000)
   return sixDigitNum.toString()
 }
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.set('view engine', 'ejs')
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.set('view engine', 'ejs')
+
 //ROUTES
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>Word</b></body></html>\n")
-})
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>Word</b></body></html>\n")
+// })
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
-})
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-
 app.get("/urls/:shortURL", (req, res) => {
+  console.log(req)
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars)
 })
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 });
 
-
+app.post("/urls", (req, res) => {
+  // const longURL = req.body.longURL
+  const shortUrl = generateRandomString()
+  console.log("body longURL: ", req.body.longURL)
+  urlDatabase[shortUrl] = req.body.longURL
+  // res.render()
+  res.redirect(`/urls/${shortUrl}`);         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
