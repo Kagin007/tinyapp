ab2104d26f728a22f8a90b0618185c10d9f0dbd1
Adam Schulte <adamschulte148@hotmail.com>
2022-01-25 21:28:01 +0000

    adding login and logout functionality

--- express_server.js
+++ express_server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 
 function generateRandomString() {
   let sixDigitNum = Math.floor(Math.random()*1000000)
@@ -19,16 +21,25 @@ app.set('view engine', 'ejs')
 //ROUTES
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase };
   res.render("urls_index", templateVars)
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars)
 })
 
@@ -52,6 +63,17 @@ app.post("/urls/:shortURL/update", (req, res) => {
   console.log(req.body.newURL)
   // urlDatabase[shortURL] = req.body
   urlDatabase[req.params.shortURL] = req.body.newURL
+  res.redirect('/urls')
+})
+
+app.post("/login", (req, res) => {
+  console.log(req.body.username)
+  res.cookie("username", req.body.username)
+  res.redirect('/urls')
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
   res.redirect('/urls')
 })
 
